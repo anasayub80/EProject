b5aa5706b57a09596501c1923e28c25407fe781f
anasayub80 <m.anasayub80@gmail.com>
2022-12-06 06:57:52 +0500

    before footer remove

--- js/code.js
+++ js/code.js
@@ -30,10 +30,23 @@ function navigate(id) {
 
     document.location.href = url;
 }
+function navigateEquipments(id) {
+ 
+        url = 'http://127.0.0.1:5500/pages/equipmentsdetail.html?id=' + encodeURIComponent(id);
+
+    document.location.href = url;
+}
+function navigateRemedies(id) {
+ 
+        url = 'http://127.0.0.1:5500/pages/remediesread.html?id=' + encodeURIComponent(id);
+
+    document.location.href = url;
+}
+
 function callHomeProducts(){
     for(var i=0;i<4;i++)
     {
-        document.getElementById("cards").innerHTML += '<div onclick="navigate('+products[i].id+')" class="card card-bg my-3 mx-3" style="width: 18rem;"><img  src="'+products[i].image+'" class="card-img-top " alt="..."><div class="card-body"><h5 class="card-title">'+products[i].name+'</h5><p class="card-text card-subtitle">'+products[i].smallDesc+'</p><p class="card-text" style="color: #bf9456;font-weight: bold; ">'+products[i].price+'</p> <a href="#" class="btn btn-secondary inverted" href="../pages/productdetail.html" style="background-color: #bf9456;">View More</a></div></div>'
+        document.getElementById("cards").innerHTML += '<div onclick="navigate('+products[i].id+')" class="card card-bg my-3 mx-3" style="width: 18rem;"><img  src="'+products[i].image+'" class="card-img-top " alt="..."><div class="card-body"><h5 class="card-title">'+products[i].title+'</h5><p class="card-text card-subtitle">'+products[i].smallDesc+'</p><p class="card-text" style="color: #bf9456;font-weight: bold; ">'+products[i].price+'</p> <a href="#" class="btn btn-secondary inverted" href="../pages/productdetail.html" style="background-color: #bf9456;">View More</a></div></div>'
     }
 }
 function callallProducts(){
@@ -45,26 +58,26 @@ function callallProducts(){
 function callHomeEquipments(){
     for(var i=0;i<4;i++)
     {
-        document.getElementById("equipment-home-cards").innerHTML += '<div onclick="navigate('+products[i].id+')" class="card card-bg my-3 mx-3" style="width: 18rem;"><img src="'+equipments[i].image+'" class="card-img-top " alt="..."><div class="card-body"><h5 class="card-title">'+equipments[i].title+'</h5><p class="card-text card-subtitle">'+equipments[i].smallDesc+'</p><p class="card-text" style="color: #bf9456;font-weight: bold; ">'+equipments[i].price+'</p> <a href="#" class="btn btn-secondary" style="background-color: #bf9456;">Go somewhere</a></div></div>'
+        document.getElementById("equipment-home-cards").innerHTML += '<div onclick="navigateEquipments('+equipments[i].id+')" class="card card-bg my-3 mx-3" style="width: 18rem;"><img src="'+equipments[i].image+'" class="card-img-top " alt="..."><div class="card-body"><h5 class="card-title">'+equipments[i].title+'</h5><p class="card-text card-subtitle">'+equipments[i].smallDesc+'</p><p class="card-text" style="color: #bf9456;font-weight: bold; ">'+equipments[i].price+'</p> <a href="#" class="btn btn-secondary" style="background-color: #bf9456;">Go somewhere</a></div></div>'
     }
 }
 function callallEquipments(){
     for(var i=0;i<equipments.length;i++)
     {
-        document.getElementById("equipment-cards").innerHTML += '<div onclick="navigate('+products[i].id+')" class="card card-bg my-3 mx-3" style="width: 18rem;"><img src="'+equipments[i].image+'" class="card-img-top " alt="..."><div class="card-body"><h5 class="card-title">'+equipments[i].name+'</h5><p class="card-text card-subtitle">'+equipments[i].smallDesc+'</p><p class="card-text" style="color: #bf9456;font-weight: bold; ">'+equipments[i].price+'</p> <a href="#" class="btn btn-secondary" style="background-color: #bf9456;">Go somewhere</a></div></div>'
+        document.getElementById("equipment-cards").innerHTML += '<div onclick="navigateEquipments('+equipments[i].id+')" class="card card-bg my-3 mx-3" style="width: 18rem;"><img src="'+equipments[i].image+'" class="card-img-top " alt="..."><div class="card-body"><h5 class="card-title">'+equipments[i].title+'</h5><p class="card-text card-subtitle">'+equipments[i].smallDesc+'</p><p class="card-text" style="color: #bf9456;font-weight: bold; ">'+equipments[i].price+'</p> <a href="#" class="btn btn-secondary" style="background-color: #bf9456;">Go somewhere</a></div></div>'
     }
 }
 function callHomeremedies(){
     for(var i=0;i<3;i++)
     {
-        document.getElementById("remedies-home-cards").innerHTML += ' <div onclick="navigate('+products[i].id+')" class="card post-card my-3 mx-3 p-0" ><div class"post-img"><img class="card-img-top img-responsive" src="'+remedies[i].image+'" alt="card image cap" title="'+remedies[i].title+'"> </div><div class="card-body"><h4 class="card-title p-2">'+remedies[i].title+'</h4><p class="card-text">'+remedies[i].smallDesc+'</p>            <p class="card-text" style="color: #bf9456;"><small>'+remedies[i].lastUpdate+'</small></p>        </div></div>'
+        document.getElementById("remedies-home-cards").innerHTML += ' <div onclick="navigateRemedies('+remedies[i].id+')" class="card post-card my-3 mx-3 p-0" ><div class"post-img"><img class="card-img-top img-responsive" src="'+remedies[i].image+'" alt="card image cap" title="'+remedies[i].title+'"> </div><div class="card-body"><h4 class="card-title p-2">'+remedies[i].title+'</h4><p class="card-text">'+remedies[i].smallDesc+'</p><p class="card-text" style="color: #bf9456;"><small>'+remedies[i].lastUpdate+'</small></p>        </div></div>'
     }
 }
  
 function callAllHomeremedies(){
     for(var i=0;i<remedies.length;i++)
     {
-        document.getElementById("remedies-cards").innerHTML += ' <div onclick="navigate('+products[i].id+')" class="card post-card my-3 mx-3 p-0" ><div class"post-img"><img class="card-img-top img-responsive" src="'+remedies[i].image+'" alt="card image cap" title="'+remedies[i].title+'"> </div><div class="card-body"><h4 class="card-title p-2">'+remedies[i].title+'</h4><p class="card-text">'+remedies[i].smallDesc+'</p>            <p class="card-text" style="color: #bf9456;"><small>'+remedies[i].lastUpdate+'</small></p>        </div></div>'
+        document.getElementById("remedies-cards").innerHTML += ' <div onclick="navigateRemedies('+remedies[i].id+')" class="card post-card my-3 mx-3 p-0" ><div class"post-img"><img class="card-img-top img-responsive" src="'+remedies[i].image+'" alt="card image cap" title="'+remedies[i].title+'"> </div><div class="card-body"><h4 class="card-title p-2">'+remedies[i].title+'</h4><p class="card-text">'+remedies[i].smallDesc+'</p> <p class="card-text" style="color: #bf9456;"><small>'+remedies[i].lastUpdate+'</small></p>        </div></div>'
     }
 }
  
